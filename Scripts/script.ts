cb71b202113fa3ae114724843367a4a026ca6788
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2023-08-06 01:08:55 +0300

    put everything in a function for creating grid

--- Scripts/script.ts
+++ Scripts/script.ts
@@ -6,39 +6,45 @@ let times = 16;
 let mouseDown = false;
 container.setAttribute('draggable', 'false')
 
+function createGrids(){
+    container.innerHTML='';
 
-for (let i=0;i<times*times;i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('grid');
-    cell.style.backgroundColor="gray";
-    
-    cell.setAttribute('draggable', 'false')
-
-    cell.style.width=container.clientWidth/times+'px';
-    cell.style.height=container.clientHeight/times+'px';
-
-    cell.addEventListener('mousemove', () => {
-        if (mouseDown) cell.classList.add('black');
+    for (let i=0;i<times*times;i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('grid');
+        cell.style.backgroundColor="gray";
         
-    });
-    cell.addEventListener('click', () => {
-       cell.classList.add('black');
-        
-    });
-
-
-    cell.addEventListener('mousedown', () => {
-        mouseDown = true;
-
-    });
-    cell.addEventListener('mouseup', () => {
-        mouseDown = false;
-
-    });
-
-    cell.addEventListener('dragstart', (e:any) => {
-        e.preventDefault();
-    });
+        cell.setAttribute('draggable', 'false')
     
-    container?.appendChild(cell);
+        cell.style.width=container.clientWidth/times+'px';
+        cell.style.height=container.clientHeight/times+'px';
+    
+        cell.addEventListener('mousemove', () => {
+            if (mouseDown) cell.classList.add('black');
+            
+        });
+        cell.addEventListener('click', () => {
+           cell.classList.add('black');
+            
+        });
+    
+    
+        cell.addEventListener('mousedown', () => {
+            mouseDown = true;
+    
+        });
+        cell.addEventListener('mouseup', () => {
+            mouseDown = false;
+    
+        });
+    
+        cell.addEventListener('dragstart', (e:any) => {
+            e.preventDefault();
+        });
+        
+        container?.appendChild(cell);
+    }
 }
+
+createGrids();
+
