a81e5b09598388099ea58d29aa38395332204784
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2023-08-06 01:05:10 +0300

    Fixed the drag Problem

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,23 +1,29 @@
 var container = document.querySelector('.container');
-var times = 64;
+var times = 16;
 var mouseDown = false;
+container.setAttribute('draggable', 'false');
 var _loop_1 = function (i) {
     var cell = document.createElement('div');
     cell.classList.add('grid');
     cell.style.backgroundColor = "gray";
+    cell.setAttribute('draggable', 'false');
     cell.style.width = container.clientWidth / times + 'px';
     cell.style.height = container.clientHeight / times + 'px';
     cell.addEventListener('mousemove', function () {
         if (mouseDown)
             cell.classList.add('black');
-        else
-            cell.classList.remove('black');
+    });
+    cell.addEventListener('click', function () {
+        cell.classList.add('black');
     });
     cell.addEventListener('mousedown', function () {
         mouseDown = true;
     });
     cell.addEventListener('mouseup', function () {
         mouseDown = false;
+    });
+    cell.addEventListener('dragstart', function (e) {
+        e.preventDefault();
     });
     container === null || container === void 0 ? void 0 : container.appendChild(cell);
 };
--- Scripts/script.ts
+++ Scripts/script.ts
@@ -1,20 +1,29 @@
 const container=document.querySelector('.container');
 
-let times = 64;
+let times = 16;
+
+
 let mouseDown = false;
+container.setAttribute('draggable', 'false')
+
 
 for (let i=0;i<times*times;i++) {
     const cell = document.createElement('div');
     cell.classList.add('grid');
     cell.style.backgroundColor="gray";
-
+    
+    cell.setAttribute('draggable', 'false')
 
     cell.style.width=container.clientWidth/times+'px';
     cell.style.height=container.clientHeight/times+'px';
 
     cell.addEventListener('mousemove', () => {
         if (mouseDown) cell.classList.add('black');
-        else cell.classList.remove('black');
+        
+    });
+    cell.addEventListener('click', () => {
+       cell.classList.add('black');
+        
     });
 
 
@@ -25,6 +34,10 @@ for (let i=0;i<times*times;i++) {
     cell.addEventListener('mouseup', () => {
         mouseDown = false;
 
+    });
+
+    cell.addEventListener('dragstart', (e:any) => {
+        e.preventDefault();
     });
     
     container?.appendChild(cell);
